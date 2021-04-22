12f0d652a6e36db7b87008e13eea9d9e538dfd0a
Flavio <fdipietro01@gmail.com>
2021-04-22 14:48:26 -0300

    img styles fixed

--- src/container/ItemCount/ItemCount.js
+++ src/container/ItemCount/ItemCount.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ItemCount.css";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import{faCaretDown, faCaretUp} from '@fortawesome/free-solid-svg-icons';
+import{faCaretDown, faCaretUp, faShoppingBasket} from '@fortawesome/free-solid-svg-icons';
 
 
 export const ItemCount = ({stock})=>{
@@ -11,11 +11,11 @@ export const ItemCount = ({stock})=>{
             <div className="counter-container">
                 <div className="botonera">
                     <button className="but butminus"><FontAwesomeIcon className="ic" icon={faCaretDown}/></button>
-                    <p>0</p>
+                    <p className="contador">0</p>
                     <button className="but"><FontAwesomeIcon className="ic" icon={faCaretUp}/></button>
                 </div>
                 <p className="stock">(Stock disponible: {stock})</p>
-                <button className="chart-button">Agregar al Carrito</button>
+                <button className="chart-button"><FontAwesomeIcon className="basket" icon={faShoppingBasket}/> Agregar al Carrito</button>
             </div>
 
         </>
