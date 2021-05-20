8e6664274e183465950ad94eed1b0f278f9c8a38
Flavio <fdipietro01@gmail.com>
2021-05-20 15:24:09 -0300

    calculador de precios done

--- src/container/CartContainer/Cart.js
+++ src/container/CartContainer/Cart.js
@@ -1,24 +1,20 @@
-import React, {useContext, useEffect, useState} from "react";
-import { CartContext } from "../../context/CartContext"
+import React, { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
 import "./Cart.css";
 import { Link } from "react-router-dom";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import{faTrash} from '@fortawesome/free-solid-svg-icons';
-import{faExternalLinkAlt} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 
 export const Cart = () => {
-  const [totalPrice, setTotalPrice] = useState(0)
-  const {cart} = useContext(CartContext);
-  const {removeIt} = useContext(CartContext);
-
- useEffect(()=>{
-    cart.length>0? cart.map(x=> setTotalPrice (totalPrice + (x.price*x.quantify))) : setTotalPrice(0)
- },[cart])
-  
+  const { cart } = useContext(CartContext);
+  const { removeIt } = useContext(CartContext);
+  const { total } = useContext(CartContext);
 
   return (
     <>
-      <div className="mainly">
+    <div className ="bodyCart">
+      <div className= {cart.length > 0? "mainly" : "emptyCart"}>
         {cart.map((x) => {
           return (
             <div key={x.id} className="CartDetailContainer">
@@ -31,20 +27,36 @@ export const Cart = () => {
                 {" "}
                 Subotal<span className="TotSpan">${x.quantify * x.price}</span>
               </div>
-              <div className = "botonera" >
-              <button  className="trash"><FontAwesomeIcon className="trashIc" icon={faTrash}/> </button>
-              <Link className= "trashLink" to={`/item/${x.id}`}><button className="trash"><FontAwesomeIcon className="trashIc" icon={faExternalLinkAlt}/> </button></Link>
+              <div className="botonera">
+                <button onClick={() => removeIt(x.id)} className="trash">
+                  <FontAwesomeIcon className="trashIc" icon={faTrash} />{" "}
+                </button>
+                <Link className="trashLink" to={`/item/${x.id}`}>
+                  <button className="trash">
+                    <FontAwesomeIcon
+                      className="trashIc"
+                      icon={faExternalLinkAlt}
+                    />{" "}
+                  </button>
+                </Link>
               </div>
-            </div>            
+            </div>
           );
         })}
 
-        {cart.length > 0 && <div> Total: {totalPrice}
-                                <div className="endMenu">
-                                    <Link to={"/"}> <button className="endMenuBut">Volver al catálogo</button></Link>
-                                    <button className="endMenuBut">Terminar compra</button>
-                                </div>
-                            </div>}
+        {cart.length > 0? (
+          <div className="bottomBox">
+            <div className="cartTotal"> Total: <span className="cartTotalValue">${total}</span></div>
+            <p className="aclaracion">*todos los impuestos agregados</p>
+            <div className="endMenu">
+              <Link to={"/"}> <button className="endMenuBut">Volver al catálogo</button> </Link>
+              <button className="endMenuBut">Terminar compra</button>
+            </div>
+          </div>
+        ) : <div className="emptyCartText">¿Todavía no encontraste lo que buscabas?
+        <p className="emptyCartText2">¡Encontrá lo mejor de nuestro catálogo acá!</p>
+        <Link to={"./"} ><div><img src="https://firebasestorage.googleapis.com/v0/b/roots-stor.appspot.com/o/root.jpg?alt=media&token=c0466596-fb53-4e60-9780-09342494a520" className="log" alt="logo" /></div></Link></div>}
+      </div>
       </div>
     </>
   );
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,10 +1,15 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 
 export const CartContext = React.createContext([]); 
 
 export const CartItems = ({children})=>{
 
+    
     const [cart, setCart] = useState([]);
+    const [total, setTotal] = useState(0);
+
+    useEffect(()=>{console.log(cart)
+    sumarTotal()},[cart])
 
     const addDuplicated = (item, quantify)=> {
         const duplicated = cart.filter(x=> x.id === item.id);
@@ -19,7 +24,7 @@ export const CartItems = ({children})=>{
     const addItem = (item, quantify)=>{
         console.log(item.id)
         console.log(isInCart(item.id))
-        isInCart(item.id)? addDuplicated(item, quantify) : setCart([...cart, {...item, id: item.id, quantify: quantify}]) 
+        isInCart(item.id)? addDuplicated(item, quantify) : setCart([...cart, {...item, id: item.id, quantify: quantify}])
     }
     
     const isInCart = (id)=>{
@@ -29,14 +34,21 @@ export const CartItems = ({children})=>{
     }
 
     const removeIt = (id)=>{
-        cart.forEach((item,idx)=>{item.id === id && setCart(cart.splice(idx,1))})   
+        setCart(cart.filter(x=>x.id !== id))   
+    }
+
+    const sumarTotal = ()=>{
+        let acum = 0
+        cart.map(x=> acum = acum + (x.price*x.quantify))
+        console.log(acum)
+        setTotal(acum)
     }
 
     const clearCart = ()=> setCart([]);
     
 
     return (
-        <CartContext.Provider value={{cart, addDuplicated, addItem, setCart, isInCart, clearCart, removeIt}}>
+        <CartContext.Provider value={{cart, addDuplicated, addItem, setCart, isInCart, clearCart, removeIt, sumarTotal, total}}>
             {children}
             </CartContext.Provider>
     )      
